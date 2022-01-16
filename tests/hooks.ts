5a7e66de300e1718aa0fb788f2d7a3a08dd0321d
Oli Radlett <o.radlett@gmail.com>
2022-01-16 19:36:21 +0000

    Add first working test

--- tests/hooks.ts
+++ tests/hooks.ts
@@ -1,9 +1,53 @@
 import db from "@db";
+import axios from "axios";
+// import app from "index";
+import mongoose from "mongoose";
 
 export const mochaHooks = {
+
   beforeAll: [
+    
+    // Connect to the database before running tests
     async function () {
       await db();
     },
+
+    async function () {
+      
+      // wipe local db
+      
+      const collections = mongoose.connection.collections
+
+      for (const key in collections) {
+
+        const collection = collections[key];
+        await collection.deleteMany({});
+
+      }
+
+    },
+
+    // Authorise the user before running tests
+    async function() {
+
+      // Create new user
+      // In the future we'll create an admin user and a normal user for the tests
+      // TODO get this url from env
+      await axios.post(`http://localhost:8080/auth/register`,
+      {
+        email: "admin@test.co.uk",
+        name: "Admin",
+        surname: "User",
+        password: "password",
+        username: "admin"
+      }).then((response) => {
+        console.log(response.status);
+        process.env.authToken = response.data.tokens.at;
+        console.log(process.env.authToken);
+      }).catch((error) => {
+        console.error(error);
+      });
+
+    }
   ],
 };
--- tests/users.test.ts
+++ tests/users.test.ts
@@ -1,18 +1,40 @@
 import request from "supertest";
 import app from "@index";
 import { expect } from "chai";
+// import  UserModel from '@models/user';
+
+
+let authToken: string;
+
+before(async () => {
+
+  if (process.env.authToken  !== undefined) {
+
+    authToken = process.env.authToken;
+
+  } else {
+
+    throw new Error("Auth token isn't set, exiting")
+
+  }
+
+});
 
 describe("Users", async () => {
 
-  it("GET /users/ returns an array", async (done) => {
+  it("GET /users/ returns an array", async () => {
 
-    await request(app)
-      .get("/users/")
+    return await request(app)
+      .get("/users/").set('Authorization', `Bearer ${authToken}`)
       .then((res) => {
-        expect(res.status).to.eql(200);
-        expect(res.body.msg).to.be.an('array');
 
-        done();
+        expect(res.status).to.eql(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body.length).to.eql(1);
+
+      }).catch((error: Error) => {
+
+        console.error(error);
 
       });
 
