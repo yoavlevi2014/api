80eeb571edf1bff655784fb1026fb9c786aaf766
JamesBeach2 <james.beach.98@gmail.com>
2022-01-31 00:04:54 +0000

    attempt to fix deepscan

--- src/helpers/socketUsers.js
+++ src/helpers/socketUsers.js
@@ -11,18 +11,19 @@ function userJoin(id, username, room) {
 
 // Get current user
 function getCurrentUser(id) {
-  return users.find(user => user.id === id);
+  return users.find(user => {return user.id === id});
 }
 
 // User leaves chat
 function userLeave(id) {
-  const index = users.findIndex(user => user.id === id);
+  const index = users.findIndex(user => {return user.id === id});
 
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
 }
 
+// eslint-disable-next-line no-undef
 module.exports = {
   userJoin,
   getCurrentUser,
