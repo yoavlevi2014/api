486585bbd6bc8cbef547bf251182612110373410
JamesBeach2 <71639303+JamesBeach2@users.noreply.github.com>
2022-03-14 13:40:35 +0000

    SocketIO room admins

--- src/helpers/socketUsers.ts
+++ src/helpers/socketUsers.ts
@@ -1,12 +1,19 @@
-const users: { id: string; username: string; room: string; }[] = [];
+const users: { id: string; username: string; room: string; admin: boolean }[] = [];
 
 // Join user to chat
-export function userJoin(id: string, username: string, room: string) {
-  const user = { id, username, room };
+export function userJoin(id: string, username: string, room: string, admin: boolean) {
+  const user = { id, username, room, admin };
 
   users.push(user);
 
   return user;
+}
+
+// Check if new user is admin to their room
+export function checkUserAdmin(room : string) {
+  const exists = users.find(user => {return room === user.room});
+
+  return exists ? false : true;
 }
 
 // Get current user
@@ -18,7 +25,7 @@ export function getCurrentUser(id : string) {
 export function userLeave(id : string) {
   const index = users.findIndex(user => {return user.id === id});
 
-  if (index !== -1) {
+  if (index !== -1 && !users[index].admin) {
     return users.splice(index, 1)[0];
   }
 }
@@ -27,5 +34,6 @@ export function userLeave(id : string) {
 module.exports = {
   userJoin,
   getCurrentUser,
-  userLeave
+  userLeave,
+  checkUserAdmin
 };
