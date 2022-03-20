8d7e6b2167df3f1cb687e69b3c998f3f2b7f0da9
JamesBeach2 <james.beach.98@gmail.com>
2022-03-20 18:02:00 +0000

    AAAAAAAAHHHHHHHHHHHHHH

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const main = async () => {
   app.use(express.json());
   app.use(morgan("tiny"));
   app.use(express.urlencoded({ extended: true }));
-  app.use(function(_req, res, next) {
+  app.use(function (_req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
@@ -69,19 +69,19 @@ const main = async () => {
   app.post("/users/friends/request/accept/:request_id", UserController.acceptFriendRequest);
   app.post("/users/friends/request/cancel/:request_id", UserController.cancelFriendRequest);
 
-  
-  // Launch socket server
-  createSocketServer();
-
   // Post routes
   app.get("/posts", PostController.getAllPosts);
   app.get("/posts/user", PostController.getPostsByUser);
   app.post("/posts", PostController.createPost);
   app.post("/posts/comment", PostController.addComment);
 
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     console.log(`listening on port ${port}`);
   });
+
+  // Launch socket server
+  createSocketServer(server);
+  
 };
 
 main();
